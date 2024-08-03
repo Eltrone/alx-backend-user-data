39941d0b92af03b3a808c51b32e50f11b1914e0d
Eltrone <amineelorche@gmail.com>
2024-08-03 22:27:45 +0000

    file added

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Module to filter PII data fields from log records.
+"""
+
+import re
+
+def filter_datum(fields, redaction, message, separator):
+    """
+    Obfuscate specific fields in a log message.
+
+    Args:
+        fields (list): List of fields to obfuscate.
+        redaction (str): The value to obfuscate the fields with.
+        message (str): The log message.
+        separator (str): Field separator in the log message.
+
+    Returns:
+        str: The obfuscated log message.
+    """
+    for field in fields:
+        message = re.sub(f"{field}=[^;]*", f"{field}={redaction}", message)
+    return message
