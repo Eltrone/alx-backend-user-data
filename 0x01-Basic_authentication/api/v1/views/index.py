ad7b358cd48f8b6bb15b819f01e9f8bddc766701
Eltrone <amineelorche@gmail.com>
2024-08-08 00:30:24 +0000

    file added

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,19 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """ GET /api/v1/unauthorized
+    This endpoint explicitly raises a 401 Unauthorized error
+    using the abort function from Flask.
+    """
+    abort(401)
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+    This endpoint explicitly raises a 403 Forbidden error
+    using the abort function from Flask.
+    """
+    abort(403)
