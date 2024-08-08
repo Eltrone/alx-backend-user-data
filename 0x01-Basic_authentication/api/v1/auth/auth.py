9b72c034e52eb231b428d4e50645239942d49ea4
Eltrone <amineelorche@gmail.com>
2024-08-08 00:53:55 +0000

    file added

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 """
-Ce module définit la classe Auth utilisée pour gérer l'authentification dans l'API.
+Ce module définit la classe Auth utilisée
+pour gérer l'authentification dans l'API.
 """
 
-
-# api/v1/auth/auth.py
 from flask import request
 from typing import List, TypeVar
 
@@ -12,14 +11,27 @@ User = TypeVar('User')  # Utilisé pour le type de retour de current_user
 
 
 class Auth:
+    """
+    Classe Auth pour gérer mécanismes d'authentification de base dans l'API.
+    """
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ Vérifie si le chemin requiert une authentification. """
-        return False  # Toujours False pour l'instant
+        """
+        Détermine si le chemin donné nécessite une authentification.
+        Retourne toujours False pour le moment.
+        """
+        return False
 
     def authorization_header(self, request=None) -> str:
-        """ Récupère l'entête d'autorisation de la requête. """
-        return None  # Toujours None pour l'instant
+        """
+        Récupère l'en-tête d'autorisation de la requête, si elle existe.
+        Retourne None pour le moment.
+        """
+        return None
 
     def current_user(self, request=None) -> User:
-        """ Récupère l'utilisateur courant de la requête. """
-        return None  # Toujours None pour l'instant
+        """
+        Identifie l'utilisateur courant à partir de la requête.
+        Retourne None pour le moment.
+        """
+        return None
