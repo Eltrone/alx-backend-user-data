6dca9905c6e55d41f9658b8863bee7efb9b40a93
Eltrone <amineelorche@gmail.com>
2024-08-08 23:04:08 +0000

    file added

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,52 @@
+import base64
+from api.v1.auth.auth import Auth
+from typing import TypeVar, Tuple
+
+User = TypeVar('User')  # Définition de type pour l'utilisateur
+
+class BasicAuth(Auth):
+    # Espace supplémentaire pour respecter E302
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        # Vérifie l'en-tête d'autorisation
+        if authorization_header is None or not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+        return authorization_header[6:]
+
+    # Décode la partie Base64 de l'en-tête
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        if base64_authorization_header is None or not isinstance(
+                base64_authorization_header, str):
+            return None
+        try:
+            base64_bytes = base64.b64decode(base64_authorization_header)
+            return base64_bytes.decode('utf-8')
+        except Exception:
+            return None
+
+    # Extrait les informations d'identification de l'utilisateur
+    def extract_user_credentials(self, decoded_base64_authorization_header: str) -> Tuple[str, str]:
+        if decoded_base64_authorization_header is None or not isinstance(
+                decoded_base64_authorization_header, str):
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+        user_email, user_pwd = decoded_base64_authorization_header.split(':', 1)
+        return user_email, user_pwd
+
+    # Récupère l'objet utilisateur à partir des informations d'identification
+    def user_object_from_credentials(self, user_email: str, user_pwd: str) -> User:
+        if user_email is None or not isinstance(user_email, str) or user_pwd is None or not isinstance(user_pwd, str):
+            return None
+        # Implémentez la logique de recherche d'utilisateur ici
+        return None
+
+    # Identifie l'utilisateur à partir de la requête
+    def current_user(self, request=None) -> User:
+        authorization_header = self.authorization_header(request)
+        base64_part = self.extract_base64_authorization_header(authorization_header)
+        decoded_part = self.decode_base64_authorization_header(base64_part)
+        email, password = self.extract_user_credentials(decoded_part)
+        user = self.user_object_from_credentials(email, password)
+        return user
