9d7dc0232abdc68dc6e59ed4495b42b70f4f60a8
Eltrone <amineelorche@gmail.com>
2024-08-08 00:56:54 +0000

    file added

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,19 +12,35 @@ User = TypeVar('User')  # Utilisé pour le type de retour de current_user
 
 class Auth:
     """
-    Classe Auth pour gérer mécanismes d'authentification de base dans l'API.
+    Classe Auth pour gérer les mécanismes d'authentification
+    de base dans l'API.
     """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
-        Détermine si le chemin donné nécessite une authentification.
-        Retourne toujours False pour le moment.
+        Détermine si le chemin donné nécessite une authentification
+        en vérifiant s'il est dans la liste des chemins exclus.
+        Retourne True si le chemin n'est pas dans la liste des
+        chemins exclus ou si certaines conditions sont vraies
+        (voir ci-dessous).
         """
-        return False
+        if path is None:
+            return True
+        if not excluded_paths:
+            return True
+        # Normaliser le chemin pour la tolérance au slash
+        path = path.strip('/') + '/'
+        for pattern in excluded_paths:
+            if pattern.endswith('/'):
+                pattern = pattern.rstrip('/')
+            if path.startswith(pattern + '/') or path == pattern + '/':
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
-        Récupère l'en-tête d'autorisation de la requête, si elle existe.
+        Récupère l'en-tête d'autorisation de la requête,
+        si elle existe.
         Retourne None pour le moment.
         """
         return None
