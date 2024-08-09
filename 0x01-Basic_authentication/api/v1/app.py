314ae261c9f8009bf2e8ad57b4f20c3b7ab1fd8f
Eltrone <amineelorche@gmail.com>
2024-08-09 23:38:15 +0000

    file added

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -6,7 +6,6 @@ from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -25,6 +24,7 @@ elif auth_type == 'auth':
 
 @app.before_request
 def before_request_func():
+    """ Before request function """
     if auth is None:
         return
     excluded_paths = [
