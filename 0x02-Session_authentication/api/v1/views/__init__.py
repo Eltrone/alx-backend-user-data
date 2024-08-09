1fb934797c4309fe07c4618b213425a700b92ddc
Eltrone <amineelorche@gmail.com>
2024-08-09 12:22:58 +0000

    file added

--- 0x02-Session_authentication/api/v1/views/__init__.py
+++ 0x02-Session_authentication/api/v1/views/__init__.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python3
-"""Initialise les vues de l'API avec Blueprint pour les routes '/api/v1'."""
-
+""" DocDocDocDocDocDoc
+"""
 from flask import Blueprint
-from api.v1.views.session_auth import *
 
-# Blueprint pour les vues de l'API, préfixé par '/api/v1'.
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
-# Importe les vues pour les endpoints index et users.
 from api.v1.views.index import *
 from api.v1.views.users import *
 
-# Charge les données utilisateur depuis un fichier.
 User.load_from_file()
