caccc14876b6e75dec15d5c842a71cbbe0c7bac3
Eltrone <amineelorche@gmail.com>
2024-08-09 21:35:56 +0000

    file added

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -39,8 +39,9 @@ def before_request_func():
         return
     if auth.authorization_header(request) is None:
         abort(401)
+    current_user = auth.current_user(request)
     request.current_user = auth.current_user(request)
-    if not auth.current_user(request):
+    if not current_user:
         abort(403)
 
 
